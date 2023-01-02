9c0e79d258554c3a2718024ea4449e1135867102
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-01-02 16:03:37 +0900

    🚀 feat: ホームページを定義

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:yutakamaru_proto/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '釣船　ゆたか丸　宮古港',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/// ホーム画面
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[],
+        ),
+      ),
+    );
+  }
+}
